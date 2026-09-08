1657cedc25a42da0380dd29798a3279a024861ba
SegaHohag <slanovsergei@gmail.com>
2026-09-08 18:43:49 +0300

    Update script.js
    Добавил отзыв Василия

--- js/script.js
+++ js/script.js
@@ -33,6 +33,17 @@ const reviews = [
     photos: ['images/honda-civic-8-otzyv-ruchka-ruchnika.jpg'],
     text: `Отличный продавец, помог в поиске дополнительных деталей для ручника, сделал скидку, всегда на связи, быстро отправил! Рекомендую однозначно!`
   },
+ {
+  name: 'Василий',
+  photos: [
+    'images/otzyv-ruchka-ruchnika-civic-5d-1.jpg',
+    'images/otzyv-ruchka-ruchnika-civic-5d-2.jpg',
+    'images/otzyv-ruchka-ruchnika-civic-5d-3.jpg',
+    'images/otzyv-ruchka-ruchnika-civic-5d-4.jpg',
+    'images/otzyv-ruchka-ruchnika-civic-5d-5.jpg'
+  ],
+  text: `Установил ручку, всё хорошо. Спасибо за фото! Резинку в основание переставили, тоже всё нормально, всё встало по месту.`
+},
   {
     name: 'Владислав, Civic 5D',
     photos: ['images/review1.jpg', 'images/review2.jpg'],
